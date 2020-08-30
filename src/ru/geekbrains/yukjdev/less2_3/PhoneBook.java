bdfb0e3af4aa4155f28970e51eca940f273b2f5e
YuKJDev <yury.a.kropotkin@gmail.com>
2020-08-30 19:01:56 +1100

    load files

---
+++ src/ru/geekbrains/yukjdev/less2_3/PhoneBook.java
@@ -0,0 +1,26 @@
+package ru.geekbrains.yukjdev.less2_3;
+
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class PhoneBook {
+
+    HashMap<String, HashSet<String>> phoneBook;
+
+    public PhoneBook() {
+        this.phoneBook = new HashMap<>();
+    }
+
+    public void addContact(String name, String phone) {
+        HashSet<String> contact = phoneBook.getOrDefault(name, new HashSet<>()); //Если есть контакт с такой же фамилией, то добавляем еще один номер телефона
+        contact.add(phone);
+        phoneBook.put(name, contact);
+    }
+
+    public void findAndPrint(String name) {
+        System.out.println("Фамилия - " + name + " / № телефона: " + phoneBook.getOrDefault(name, new HashSet<>()));
+    }
+
+
+}
